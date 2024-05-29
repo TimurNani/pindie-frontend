9db1eb2414fb9a407a78e03b3689989a13431fd7
TimurNani <167556887+TimurNani@users.noreply.github.com>
2024-05-29 18:56:48 +0300

    Правка

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindie-backend.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://ya-praktikum-bakend.nomorepartiesco.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
